b02e23b949ef2b2e62eecf122ca10c54294e2a94
Jamal S <deviouspulsar@gmail.com>
2024-09-19 11:24:40 -0600

    Basis of Resource System and Signal Package
    Created ResourceSystem to handle gaining and spending resources
    Imported https://github.com/yankooliveira/signals to be used as a generic signaling framework

---
+++ OutOfThisWorld/Assets/_Scripts/ResourceSystem.cs
@@ -0,0 +1,48 @@
+using UnityEngine;
+using deVoid.Utils;
+
+namespace OutOfThisWorld
+{
+    public class ResourceCountChanged : ASignal<ResourceSystem> {}
+
+    public class ResourceSystem : MonoBehaviour
+    {
+        /* ----------| Component Properties |---------- */
+
+            public float InitalResourceCount = 100f;
+
+        /* ----------| Instance Variables |---------- */
+
+            private float _currentResourceCount = 0;
+
+        /* ----------| Initalization Functions |---------- */
+
+            void Start()
+            {
+                _currentResourceCount = InitalResourceCount;
+            }
+
+        /* -----------| Resource Count Modification Functions |---------- */
+
+            public float GetResourceCount() { return _currentResourceCount; }
+            
+            public bool AddResources(float delta)
+            {
+                if (delta <= 0) { return false; }
+
+                _currentResourceCount += delta;
+                Signals.Get<ResourceCountChanged>().Dispatch(this);
+                return true;
+            }
+
+            public bool SpendResources(float delta)
+            {
+                if (delta <= 0) { return false; }
+                if (delta > _currentResourceCount) { return false; }
+
+                _currentResourceCount -= delta;
+                Signals.Get<ResourceCountChanged>().Dispatch(this);
+                return true;
+            }
+    }
+}
